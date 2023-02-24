490fa542339030ed3933ed9653b49414d94bb4a3
SCARRAIDER <rsahanediriweera@gmail.com>
2023-02-24 23:21:23 +0530

    created datacontext class

---
+++ WADbackend/MainDatabase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WADbackend.Models;
+
+namespace WADbackend
+{
+    public class MainDatabase:DbContext 
+    {
+        public MainDatabase(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public MainDatabase()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Seller> sellers { get; set; }
+
+        public DbSet<Buyer> buyer { get; set; }
+
+        public DbSet<Ticket> ticket { get; set; }
+    }
+}

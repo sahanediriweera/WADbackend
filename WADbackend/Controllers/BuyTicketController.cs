6e967bccfa86a5941abd09ffd4d05d67320d4b95
SCARRAIDER <rsahanediriweera@gmail.com>
2023-02-26 20:36:01 +0530

    created buy ticket controller

---
+++ WADbackend/Controllers/BuyTicketController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WADbackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BuyTicketController : Controller
+    {
+        [HttpPost]
+        [Route("buyticket")]
+        public async Task<IActionResult> BuyTicket()
+        {
+            return Ok();
+        }
+    }
+}

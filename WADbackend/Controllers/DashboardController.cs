ad33a9b3c6e8a9acf5c0819ff21e8d532d2bb89e
SCARRAIDER <rsahanediriweera@gmail.com>
2023-02-26 23:29:25 +0530

    initiated dashboard controller

---
+++ WADbackend/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WADbackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

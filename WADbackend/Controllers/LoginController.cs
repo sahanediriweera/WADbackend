1eb67d0c2d9c93e8403caeb7dd85f5e56a4d1310
SCARRAIDER <rsahanediriweera@gmail.com>
2023-03-15 00:56:12 +0530

    completed login with token

--- WADbackend/Controllers/LoginController.cs
+++ WADbackend/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -12,17 +13,39 @@ namespace WADbackend.Controllers
     public class LoginController : Controller
     {
 
-        private readonly MainDatabase _mainDatabase;
+        private readonly MainDatabase mainDatabase;
 
         public LoginController(MainDatabase mainDatabase)
         {
-            _mainDatabase = mainDatabase;
+            this.mainDatabase = mainDatabase;
         }
 
         [HttpPost]
         [Route("loginbuyer")]
-        public async Task<IActionResult> LoginBuyer()
+        public async Task<IActionResult> LoginBuyer(Login login)
         {
+            List<Buyer> buyers = await this.mainDatabase.buyers.ToListAsync();
+
+            if(buyers == null)
+            {
+                return BadRequest();
+            }
+
+            Buyer buyer = null;
+
+            foreach(var buyer1 in buyers)
+            {
+                if ((buyer1.Email == login.Email) && buyer1.Password == login.Password)
+                {
+                    buyer = buyer1;
+                }
+            }
+
+            if(buyer == null)
+            {
+                return NotFound();
+            }
+
             var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
             var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
             var tokeOptions = new JwtSecurityToken(
@@ -39,9 +62,41 @@ namespace WADbackend.Controllers
         [HttpPost]
         [Route("loginseller")]
 
-        public async Task<IActionResult> LoginSeller()
+        public async Task<IActionResult> LoginSeller(Login login)
         {
-            return Ok();
+            List<Seller> sellers = await this.mainDatabase.sellers.ToListAsync();
+
+            if(sellers == null)
+            {
+                return BadRequest();
+            }
+
+            Seller seller = null;
+
+            foreach(var seller1 in sellers)
+            {
+                if ((seller1.Email == login.Email) && seller1.Password == login.Password)
+                {
+                    seller = seller1;
+                }
+            }
+
+            if(seller == null)
+            {
+                return NotFound();
+            }
+
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+            var tokeOptions = new JwtSecurityToken(
+                issuer: "https://localhost:5001",
+                audience: "https://localhost:5001",
+                claims: new List<Claim>(),
+                expires: DateTime.Now.AddMinutes(5),
+                signingCredentials: signinCredentials
+            );
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+            return Ok(new AuthenticatedResponse { token = tokenString });
         }
     }
 }
---
+++ WADbackend/Models/Login.cs
@@ -0,0 +1,9 @@
+﻿namespace WADbackend.Models
+{
+    public class Login
+    {
+        public String Email { get; set; }
+
+        public String Password { get; set; }
+    }
+}
